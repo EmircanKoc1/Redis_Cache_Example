64cfd9fc715d10c2e8f929b78fb047395d181218
Emircan <emircandv787@gmail.com>
2024-04-10 15:41:18 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            //await _distributedCache.SetStringAsync("namekey", name);
+            //await _distributedCache.SetAsync("surnamekey", Encoding.UTF8.GetBytes(surname));
+
+            await _distributedCache.SetStringAsync("namekey", name, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+
+            await _distributedCache.SetAsync(
+                key: "namekey",
+                value: Encoding.UTF8.GetBytes(surname),
+                options: new()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                    SlidingExpiration = TimeSpan.FromSeconds(5)
+                });
+
+
+            return Ok();
+
+        }
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var surname = await _distributedCache.GetStringAsync("surname");
+            var nameBinary = await _distributedCache.GetAsync("name");
+            var name = Encoding.UTF8.GetString(nameBinary);
+
+            return Ok(name + surname);
+
+        }
+
+        [NonAction]
+        public void TryRedis()
+        {
+            using var connection = ConnectionMultiplexer.Connect("");
+            var db = connection.GetDatabase();
+
+
+
+        }
+
+
+    }
+}
---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get()
+        {
+
+
+            return Ok(_memoryCache.Get<string>("name"));
+
+        }
+        [HttpGet("[action]")]
+        public void Set()
+        {
+
+
+            _memoryCache.Set("name", "emir");
+        }
+        [HttpGet("[action]")]
+        public IActionResult SetandGetAbsoluteSlide()
+        {
+            //_memoryCache.GetOrCreate("date", entry =>
+            //{
+            //    //slide edile edile cache süresi uzayacak ama 30 oldugunda daha uzamaz , eğer vermeseydik sonsuza kadar uzardı
+            //    entry.AbsoluteExpiration = DateTime.Now.AddSeconds(30);
+
+            //    //slide edile edile cache süresi uzatılıyor
+            //    entry.SlidingExpiration = TimeSpan.FromSeconds(5);
+            //    DateTime value = DateTime.Now;
+            //    return value;
+
+            //});
+
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                //slide edile  cache süresi uzayacak ama 30 oldugunda daha uzamaz , eğer vermeseydik sonsuza kadar uzardı
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+
+                //slide edile edile cache süresi uzatılıyor
+                SlidingExpiration = TimeSpan.FromSeconds(5),
+            });
+
+            return Ok(_memoryCache.Get<DateTime>("date"));
+
+        }
+
+
+    }
+}
